12e87998f882f0584854093023592892147138b1
John Ware <johnware@Johns-MacBook-Air.local>
2017-05-24 18:06:09 -0700

    refactered web scraper. still need to add director

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const request = require('request');
 const cheerio = require('cheerio');
 const app = express();
 
-const URL = 'http://www.imdb.com/title/tt5052448/?ref_=fn_al_tt_1'
-const TITLE = '.title_wrapper > h1[itemprop="name"]'
-const RELEASE_DATE = '.title_wrapper > h1[itemprop="name"] a'
+const URL = 'http://www.imdb.com/movies-coming-soon/?ref_=inth_cs'
+const TITLE = '.list.detail .list_item.odd'
+const RELEASE_DATE = '.overview-top > h4[itemprop="name"] a'
+
 
 
 app.get('/scrape', function(req, res) {
@@ -14,30 +15,52 @@ app.get('/scrape', function(req, res) {
   request( URL, function(error, response, html) {
     if (!error) {
       const $ = cheerio.load(html);
-      const json = {}
+      const allMovies = []
 
-      $( TITLE ).each(function() {
-        const title = $(this).text().trim()
+      // $( 'h4' ).each(function(index, element) {
+      //   const title = $(this).text().trim()
+      //   const parseTitle = title.substring( 0, title.indexOf( '(' ) - 1 )
+      //
+      //   const releaseDate = $(this).text().trim()
+      //   const parseReleaseDate = releaseDate.substring( releaseDate.indexOf( '(' ) + 1, releaseDate.indexOf( ')' ) )
+      //   let movie = {title: parseTitle, releaseDate: parseReleaseDate}
+      //
+      //   allMovies.push(movie)
+      //
+      // })
 
-        json.title = title.substring( 0, title.indexOf( '(' ) - 1 )
+      $('tr').each(function(index, element) {
+
+          let h4 = $(element).find('h4[itemprop=name] > a')
+          const h4Text = $(h4).text().trim()
+
+          const title = h4Text.substring( 0, h4Text.indexOf( '(' ) - 1 )
+          const releaseDate = h4Text.substring( h4Text.indexOf( '(' ) + 1, h4Text.indexOf( ')' ) )
+
+          let director = $(element).find('div.txt-block a')
+          console.log('director::', director);
+
+          let movie = {title, releaseDate}
+
+          allMovies.push(movie)
       })
 
-      $( RELEASE_DATE ).filter(function() {
-        json.releaseDate = $(this).text();
-      })
+      let ratingsArr = []
 
-      $('.subtext > meta').each(function() {
-        json.rating = $(this).attr('content');
-      })
+      console.log('allMovies::', allMovies)
+      //
+      // $('.subtext > meta').each(function() {
+      //   json.rating = $(this).attr('content');
+      // })
+      //
+      // $('.poster').filter(function() {
+      //   json.moviePoster = $(this).find('img').attr('src');
+      // })
 
-      $('.poster').filter(function() {
-        json.moviePoster = $(this).find('img').attr('src');
-      })
 
-
-    fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err) {
-    console.log('File successfully written! - check your project directory for the output.json file')
-  })
+    fs.writeFile('output.json', JSON.stringify(allMovies, null, 4), function(err) {
+      console.log('File successfully written! - check your project directory for the output.json file')
+    })
 }
   res.send('Check your console!')
 
---
+++ tutorialCode2.js
@@ -0,0 +1,8 @@
+// DOM & DOM Events
+
+
+// 1 grab elements from the DOM
+// 2 attach event listeners
+
+const body = document.querySelector('body');
+body.style.backgroundColor = '#C00C00';
