ee6a14ae9ecbf583c403627a969fdb59cfc761d1
John Ware <johnware@Johns-MacBook-Air.local>
2017-05-25 11:45:32 -0700

    all fucntions working, next is ui

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const cheerio = require('cheerio');
 const app = express();
 
 const URL = 'http://www.imdb.com/movies-coming-soon/?ref_=inth_cs'
-const TITLE = '.list.detail .list_item.odd'
-const RELEASE_DATE = '.overview-top > h4[itemprop="name"] a'
-
-
 
 app.get('/scrape', function(req, res) {
 
@@ -16,18 +12,6 @@ app.get('/scrape', function(req, res) {
     if (!error) {
       const $ = cheerio.load(html);
       const allMovies = []
-
-      // $( 'h4' ).each(function(index, element) {
-      //   const title = $(this).text().trim()
-      //   const parseTitle = title.substring( 0, title.indexOf( '(' ) - 1 )
-      //
-      //   const releaseDate = $(this).text().trim()
-      //   const parseReleaseDate = releaseDate.substring( releaseDate.indexOf( '(' ) + 1, releaseDate.indexOf( ')' ) )
-      //   let movie = {title: parseTitle, releaseDate: parseReleaseDate}
-      //
-      //   allMovies.push(movie)
-      //
-      // })
 
       $('tr').each(function(index, element) {
 
@@ -37,38 +21,34 @@ app.get('/scrape', function(req, res) {
           const title = h4Text.substring( 0, h4Text.indexOf( '(' ) - 1 )
           const releaseDate = h4Text.substring( h4Text.indexOf( '(' ) + 1, h4Text.indexOf( ')' ) )
 
-          let director = $(element).find('div.txt-block a')
+          let a = $(element).find('div.txt-block span[itemprop=director] a')
+          const aText =$(a).text()
+          const director = aText
           console.log('director::', director);
 
-          let movie = {title, releaseDate}
+          let div = $(element).find('div.outline')
 
-          allMovies.push(movie)
+          const divText = $(div).text().trim()
+          const synopsis = divText
+
+          let img = $(element).find('div.image img')
+          const imgPic = $(img).attr('src')
+          const moviePoster = imgPic
+
+          let movie = {title, releaseDate, director, synopsis, moviePoster}
+
+          if(director.length > 0){
+            allMovies.push(movie)
+          }
       })
-
-      let ratingsArr = []
-
-      console.log('allMovies::', allMovies)
-      //
-      // $('.subtext > meta').each(function() {
-      //   json.rating = $(this).attr('content');
-      // })
-      //
-      // $('.poster').filter(function() {
-      //   json.moviePoster = $(this).find('img').attr('src');
-      // })
-
 
     fs.writeFile('output.json', JSON.stringify(allMovies, null, 4), function(err) {
       console.log('File successfully written! - check your project directory for the output.json file')
     })
-}
+  }
   res.send('Check your console!')
-
   });
-
 })
-
-
 app.listen('8081')
 console.log('Magic happens on port 8081');
 exports = module.exports = app;
