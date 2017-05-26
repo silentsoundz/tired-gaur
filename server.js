c7856d91be4b5c3b23c1b223d00f79a535885fc1
John Ware <johnware@Johns-MacBook-Air.local>
2017-05-26 15:25:39 -0700

    final product minus content from other sites

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-const express = require('express');
-const fs = require('fs');
-const request = require('request');
-const cheerio = require('cheerio');
-const app = express();
+const express = require('express')
+const fs = require('fs')
+const request = require('request')
+const cheerio = require('cheerio')
+const app = express()
+const path = require('path')
+
+app.use(express.static(path.join(__dirname + 'views')))
+app.set('view engine', 'ejs')
+
+let allMovies = []
+let movie
 
 const URL = 'http://www.imdb.com/movies-coming-soon/?ref_=inth_cs'
 
@@ -10,45 +17,42 @@ app.get('/scrape', function(req, res) {
 
   request( URL, function(error, response, html) {
     if (!error) {
-      const $ = cheerio.load(html);
-      const allMovies = []
+      const $ = cheerio.load(html)
+      // const allMovies = []
 
       $('tr').each(function(index, element) {
 
-          let h4 = $(element).find('h4[itemprop=name] > a')
-          const h4Text = $(h4).text().trim()
+        let h4 = $(element).find('h4[itemprop=name] > a')
+        const h4Text = $(h4).text().trim()
 
-          const title = h4Text.substring( 0, h4Text.indexOf( '(' ) - 1 )
-          const releaseDate = h4Text.substring( h4Text.indexOf( '(' ) + 1, h4Text.indexOf( ')' ) )
+        const title = h4Text.substring( 0, h4Text.indexOf( '(' ) - 1 )
+        const releaseDate = h4Text.substring( h4Text.indexOf( '(' ) + 1, h4Text.indexOf( ')' ) )
 
-          let a = $(element).find('div.txt-block span[itemprop=director] a')
-          const aText =$(a).text()
-          const director = aText
-          console.log('director::', director);
+        let a = $(element).find('div.txt-block span[itemprop=director] a')
+        const aText =$(a).text()
+        const director = aText
 
-          let div = $(element).find('div.outline')
 
-          const divText = $(div).text().trim()
-          const synopsis = divText
+        let div = $(element).find('div.outline')
 
-          let img = $(element).find('div.image img')
-          const imgPic = $(img).attr('src')
-          const moviePoster = imgPic
+        const divText = $(div).text().trim()
+        const synopsis = divText
 
-          let movie = {title, releaseDate, director, synopsis, moviePoster}
+        let img = $(element).find('div.image img')
+        const imgPic = $(img).attr('src')
+        const moviePoster = imgPic
 
-          if(director.length > 0){
-            allMovies.push(movie)
-          }
+        movie = {title, releaseDate, director, synopsis, moviePoster}
+
+        if(director.length > 0){
+          allMovies.push(movie)
+          return allMovies
+        }
       })
-
-    fs.writeFile('output.json', JSON.stringify(allMovies, null, 4), function(err) {
-      console.log('File successfully written! - check your project directory for the output.json file')
-    })
-  }
-  res.send('Check your console!')
-  });
+      fs.writeFile('output.json', JSON.stringify(allMovies, null, 4))
+    }
+    res.render('index', {allMovies})
+  })
 })
 app.listen('8081')
-console.log('Magic happens on port 8081');
-exports = module.exports = app;
+exports = module.exports = app
--- tutorialCode2.js
+++
@@ -1,8 +0,0 @@
-// DOM & DOM Events
-
-
-// 1 grab elements from the DOM
-// 2 attach event listeners
-
-const body = document.querySelector('body');
-body.style.backgroundColor = '#C00C00';
